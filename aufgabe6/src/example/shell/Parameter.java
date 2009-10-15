b73e9c3f15e4137e0e4d5bac4a30f8418ece0071
stzschoppe <stzschoppe@f2905fa4-b975-11de-82ee-51df2961d3cf>
2009-10-15 15:07:53 +0000

    Parameterklassen mit Leben gefüllt

--- aufgabe6/src/example/shell/Parameter.java
+++ aufgabe6/src/example/shell/Parameter.java
@@ -2,5 +2,19 @@ package shell;
 
 public abstract class Parameter {
 	private String name;
-	public abstract void setValue();
+	public abstract void setValue(String value);
+	public abstract int getValueAsInteger();
+	public abstract String getValueAsString();
+	
+	public Parameter(String name) {
+		this.setName(name);
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
